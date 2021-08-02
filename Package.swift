e0c8ee60f3d1754705ffee07e208f05c322ecfd3
Kishore <kishore.balasa@gmail.com>
2021-08-03 00:14:08 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "UITextView+Placeholder",
+    name: "UITextViewPlaceholder",
     products: [
         .library(
             name: "UITextViewPlaceholder",
